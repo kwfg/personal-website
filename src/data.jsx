68fabc523e532a7e365b99471fdf2b28a1052b1e
garyfung221 <garyfung266@gmail.com>
2023-04-04 06:09:51 +0800

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -5,7 +5,7 @@ export default [
     title: "BlogSiem",
     description:
       "Blog content management system (CMS) with the CRUD features. In addition, through simple Security information and event management (SIEM) to monitoring the whole website (collect logs, events), and detected Cross-site scripting (XSS), SQL Injection, Spam script. Then process some analysis and catch visitor details information.",
-    githubUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    githubUrl: "https://github.com/gakary/blogSiem",
     liveUrl: "http://garygary.co/index_real.php",
     skill: "PHP",
   },
