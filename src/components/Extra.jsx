cbdef1558ba26f5ea6b74ea64ca84c8f3fe1393b
Gary Fung <66684175+kwfg@users.noreply.github.com>
2025-05-20 06:09:32 +0800

    Update Extra.jsx
    update the github link

--- src/components/Extra.jsx
+++ src/components/Extra.jsx
@@ -4,7 +4,7 @@ import Github from "../images/github-ani.gif";
 function Extra() {
   return (
     <div className="fixed-right">
-      <a href="https://github.com/gakary" target="_blank">
+      <a href="https://github.com/kwfg" target="_blank">
         <img src={Github} id="fixed-image"></img>
       </a>
     </div>
